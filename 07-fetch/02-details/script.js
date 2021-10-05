71fadf5384fc632d5fddd7ce5d698912bb8df27c
binayak <69388246+itsme17@users.noreply.github.com>
2021-10-05 14:10:06 +0200

    js commit 05/10

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -11,4 +11,29 @@
 
 (() => {
     // your code here
+    var button = document.getElementById('run');
+    var input = document.getElementById('hero-id').value;
+
+    var fetched = async () => {
+        await fetch('../../_shared/api.json')
+            .then(result => result.json())
+            .then(data => {
+                button.addEventListener('click', () => {
+                    data.heroes.map(obj => {
+                        if (input == obj.id) {
+                            const template = obj;
+                            console.log(template)
+                            let xPerson = document.getElementById('tpl-hero').content.cloneNode(true);
+                            xPerson.querySelector('.name').innerText = template.name;
+                            xPerson.querySelector('.alter-ego').innerText = template.alterEgo;
+                            xPerson.querySelector('.powers').innerText = template.abilities;
+                            let target = document.getElementById("target");
+                            target.appendChild(xPerson);
+                        }
+                    })
+                })
+            })
+    }
+    fetched();
+
 })();
